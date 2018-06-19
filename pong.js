a3df850a3d3f44f052540cce1514c308ab8eac07
Anubhav Kumar <anubhav253kumar@gmail.com>
2018-06-19 17:12:39 +0530

    Refactoring the code in single function

--- pong.js
+++ pong.js
@@ -30,41 +30,51 @@ class Ball extends Rect{
         return this.pos.y + this.size.y / 2;
     }
 }
+class Pong{
+    constructor(canvas){
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        this.ball = new Ball;
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50;
+
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
+
+        let lastTime;
+        const callback = (millis) => {
+            if (lastTime) {
+                this.update((millis - lastTime) / 1000);
+            }
+            lastTime = millis;
+            requestAnimationFrame(callback);
+        };
+        callback();
+    }
+    draw(){
+        this._context.fillStyle = '#000';
+        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+        
+        this.drawRect(this.ball);
+    }
+    drawRect(rect){
+        this._context.fillStyle = '#fff';
+        this._context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
+    }
+    update(dt) {
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            this.ball.vel.x = -this.ball.vel.x;
+        }
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.vel.y = -this.ball.vel.y;
+        }
+        this.draw();
+    }
+}
 
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-const ball = new Ball;
-ball.pos.x = 100;
-ball.pos.y = 50;
-
-ball.vel.x = 100;
-ball.vel.y = 100;
-
-let lastTime;
-function callback(millis){
-    if(lastTime){
-        update((millis - lastTime) / 1000);
-    }
-    lastTime = millis;
-    requestAnimationFrame(callback);
-}
-
-function update(dt){
-    ball.pos.x += ball.vel.x * dt;
-    ball.pos.y += ball.vel.y * dt;
-
-    if(ball.left < 0 || ball.right > canvas.width){
-        ball.vel.x = -ball.vel.x;
-    }
-    if (ball.top < 0 || ball.bottom > canvas.height) {
-        ball.vel.y = -ball.vel.y;
-    }
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = '#fff';
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
-
-callback();
+const pong = new Pong(canvas);
